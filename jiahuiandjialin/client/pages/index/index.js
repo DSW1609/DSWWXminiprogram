5194a75651d7832fd0a155fc582c30c206099086
DSW1609 <DSW7296666@163.com>
2019-05-28 22:31:03 +0800

    5-28 2230

--- jiahuiandjialin/client/pages/index/index.js
+++ jiahuiandjialin/client/pages/index/index.js
@@ -14,6 +14,7 @@ Page({
         img1sjs: 0,
         img2sjs: 0,
         img3sjs: 0,
+        tximgtno:'tximgt',
         sjimg1:'sj1',
         sjimg2: 'sj1',
         sjimg3: 'sj1',
@@ -57,28 +58,33 @@ Page({
       }
   },
     alltop: function(){
-    var top1 = this.data.top1;
-    var top2 = this.data.top2;
-    var top3 = this.data.top3;
-    var sjimg1 = this.data.sjimg1;
-    var sjimg2 = this.data.sjimg2;
-    var sjimg3 = this.data.sjimg3;
     var sjimgarr = this.data.sjimgarr;
+    var top1 = this.data.top1;
     var img1sjs = this.data.img1sjs= Math.round(Math.random() * 11);
     var img2sjs = this.data.img2sjs = Math.round(Math.random() * 11);
     var img3sjs = this.data.img3sjs = Math.round(Math.random() * 11);
         this.setData({ top1: -1100 }),
         this.setData({ top2: -1100 }),
-        this.setData({ top3: -1100 })
-        if (top1 == -1100){
+        this.setData({ top3: -1100 }),
         this.setData({ sjimg1: sjimgarr[img1sjs] }),
         this.setData({ sjimg2: sjimgarr[img2sjs] }),
         this.setData({ sjimg3: sjimgarr[img3sjs] })
-        }
+        if (top1 == -1100) {
+        this.setData({ tximgtno: 'tximgt' })
+      }
   },
     allbtm: function () {
+      var tximgtno = this.data.tximgtno;
       this.setData({ top1: 0 }),
       this.setData({ top2: 0 }),
-      this.setData({ top3: 0 })
+      this.setData({ top3: 0 }),
+      this.setData({ tximgtno: 'tximgt' })
+  },
+    allstop:function(){
+      var tximgtno = this.data.tximgtno;
+      var top1 = this.data.top1;
+      if (top1 == -1100){
+        this.setData({tximgtno:'tximgtno'})
+      }
   }
 })
